407eb3fa9fc1f402ed71cedc91dfda22983e481a
Amesime <86676432+amesime-k@users.noreply.github.com>
2022-07-01 23:15:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Band Name generator
+print("Welcome to the band name creator")
+city_name = input("Which city were you born?\n")
+pet_name = input("What's your pet's name?\n")
+band_name =print(city_name + " " + pet_name)
